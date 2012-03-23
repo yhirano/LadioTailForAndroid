27a420ee53601ce5106f402160a3c8aedba56759
Y.Hirano <chirano@uraroji.com>
2012-03-23 19:28:29 +0900

    必要のないimportを削除

--- src/com/uraroji/garage/android/ladiotail/MainActivity.java
+++ src/com/uraroji/garage/android/ladiotail/MainActivity.java
@@ -27,7 +27,6 @@ import android.app.TabActivity;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.DialogInterface.OnCancelListener;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
